61c5ba868b1eb33a0f42fa8825cbdccbb109d8cb
NAVEEN <NAVEEN@DESKTOP-EFRDOCF>
2022-09-09 18:26:45 +0530

    Completed Task_NaveenDEMO

--- src/main/java/TC/TC_02_LoginFunction.java
+++ src/main/java/TC/TC_02_LoginFunction.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import POM.LoginPagePOM;
 
 
 
@@ -19,10 +19,14 @@ public class TC_02_LoginFunction extends TC_BASE
 	@Test
 	public void loginFunction() throws IOException
 	{
-		LoginPagePOM hp = new LoginPagePOM(driver);
-		hp.user();
-		hp.pass();
-		hp.log();
+		WebElement user = driver.findElement(By.xpath("//input[@id='username']"));
+		user.sendKeys("NaveenDudhyal");
+		
+		WebElement pass = driver.findElement(By.xpath("//input[@id='password']"));
+		pass.sendKeys("danceislife");
+		
+		WebElement log = driver.findElement(By.xpath("//button[@id='log-in']"));
+		log.click();
 		
 		TakesScreenshot ts = (TakesScreenshot)driver;
 		File SFile = ts.getScreenshotAs(OutputType.FILE);
--- src/main/java/TC/TC_03_AmountFunction.java
+++ src/main/java/TC/TC_03_AmountFunction.java
@@ -1,23 +1,18 @@
 package TC;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import POM.HomePagePOM;
-import POM.LoginPagePOM;
 
 public class TC_03_AmountFunction extends TC_BASE
 {
 	@Test
 	public void amountFunction()
 	{
-		LoginPagePOM lp = new LoginPagePOM(driver);
-		lp.user();
-		lp.pass();
-		lp.log();
-		
-		HomePagePOM hp = new HomePagePOM(driver);
-		hp.amount();
+		WebElement amount = driver.findElement(By.xpath("//th[@id='amount']"));
+		amount.click();
 		
 	    ((JavascriptExecutor)driver).executeScript("scroll(0,200)");
 	    
